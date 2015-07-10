ebde35362a376d6207557cf1915b26604280230f
Cory the Ninja <zephyr834@gmail.com>
2015-07-09 17:11:34 -0700

    Updated articles to include New article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,26 @@ class ArticlesController < ApplicationController
 
   end
 
+  def create
+    @article = Article.new(article_parameters)
+  end
+
+  def show
+    
+  end
+
   def index
 
   end
+
+private
+  
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_parameters
+    params.require(:article).permit(:title, :body)
+  end
+
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,14 +1,15 @@
 require "test_helper"
 
 describe Article do
-  let(:article) { Article.new }
 
   it "must be valid" do
+    article = articles(:article_one)
     value(article).must_be :valid?
   end
 
-  it "must fail" do
-    assert false
+  it "should not save without title" do
+    article = Article.new
+    assert_not article.save
   end
 
 end
